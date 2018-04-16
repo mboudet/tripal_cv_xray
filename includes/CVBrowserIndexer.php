33b36a1731ef01f033994853666d0b80e7b5b5f5
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-04-16 15:47:18 -0400

    Find the memory leak

--- includes/CVBrowserIndexer.php
+++ includes/CVBrowserIndexer.php
@@ -159,8 +159,8 @@ class CVBrowserIndexer {
     }, $entities);
 
     // Get data
-    $cvterms = $this->loadCVTerms($bundle->data_table, $record_ids);
-    $properties = $this->loadProperties($bundle->data_table, $record_ids);
+    $cvterms = &$this->loadCVTerms($bundle->data_table, $record_ids);
+    $properties = &$this->loadProperties($bundle->data_table, $record_ids);
 
     // Index by record id
     foreach ($entities as $entity) {
