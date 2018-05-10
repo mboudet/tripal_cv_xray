b0de8379781e497d26fa44df3ea17d75239ef9b3
Bradford Condon <bradford.condon@gmail.com>
2018-05-10 18:04:51 -0400

    first stab at test.  so slow to populate index locally

--- tests/Data__OntologyDataTest.php
+++ tests/Data__OntologyDataTest.php
@@ -3,9 +3,66 @@
 use StatonLab\TripalTestSuite\TripalTestCase;
 use StatonLab\TripalTestSuite\DBTransaction;
 
-class entity_child_cvterm_test extends TripalTestCase {
+require_once(__DIR__ . '/../includes/TripalFields/data__ontology_data/data__ontology_data.inc');
+
+
+class Data__OntologyDataTest extends TripalTestCase {
 
   use DBTransaction;
 
-  
+  /**
+   * @group working
+   */
+  public function test_class_loads() {
+
+    //create and publish an organism
+
+    $organism = factory('chado.organism')->create(['genus' => 'retrieve_me']);
+
+    $org_term = tripal_load_term_entity([
+      'vocabulary' => 'OBI',
+      'accession' => '0100026',
+    ]);
+    $org_bundle = tripal_load_bundle_entity(['term_id' => $org_term->id]);
+
+    tripal_chado_publish_records(['bundle_name' => $org_bundle->name]);
+
+    $mrna_term = tripal_load_term_entity([
+      'vocabulary' => 'SO',
+      'accession' => '0000234'
+    ]);
+    $mrna_bundle = tripal_load_bundle_entity([
+      'term_id' =>$mrna_term->id
+    ]);
+    //these dont work in this context!!!!
+    //    $fif = field_info_field('data__ontology_data');
+    //    $fei = field_info_instance('TripalEntity', 'data__ontology_data', $org_bundle->name);
+
+    $field_name = 'data__ontology_data';
+    $fif = [];
+    $fif['field_name'] == $field_name;
+
+    $fei = [];
+    $fei['settings']['container'] = [
+      'ontology' => 'GO',
+      'target_bundle' => $mrna_bundle->id];
+
+    //get entity id
+    $query = db_select('public.chado_' . $org_bundle->name, 'cbdx');
+    $query->fields('cbdx', ['entity_id']);
+    $query->condition('record_id', $organism->organism_id, '=');
+    $entity_id = $query->execute()->fetchField();
+
+    $entity = tripal_load_entity('TripalEntity', [$entity_id]);
+
+    $entity = $entity[$entity_id];
+
+    $obj = new data__ontology_data($fif, $fei);
+    $obj->load($entity);
+
+    //but field hasnt attached to entity.... and how could it?  we dont pass by ref.
+
+  }
+
+
 }
--- tests/TripalCVXrayAPITest.php
+++ tests/TripalCVXrayAPITest.php
@@ -1,12 +1,101 @@
 <?php
-//
-//use StatonLab\TripalTestSuite\TripalTestCase;
-//use StatonLab\TripalTestSuite\DBTransaction;
-//
-//class entity_child_cvterm_test extends TripalTestCase {
-//
-//  use DBTransaction;
-//
-//
-//
-//}
+
+use StatonLab\TripalTestSuite\TripalTestCase;
+use StatonLab\TripalTestSuite\DBTransaction;
+
+class TripalCVXrayAPITest extends TripalTestCase {
+
+  use DBTransaction;
+
+  /**
+   * @group new
+   */
+  public function test_tripal_cv_xray_lookup_entities_for_terms_count() {
+
+    //first load in organism and mRNA with some SO terms.
+
+    $mrna_id = db_select("chado.cvterm", "CVT")
+      ->fields('CVT', ['cvterm_id'])
+      ->condition('CVT.name', 'mRNA')
+      ->execute()->fetchField();
+
+
+    $mrna_term = tripal_load_term_entity([
+      'vocabulary' => 'SO',
+      'accession' => '0000234',
+    ]);
+    $mrna_bundle = tripal_load_bundle_entity([
+      'term_id' => $mrna_term->id,
+    ]);
+
+    $organism = factory('chado.organism')->create();
+    $org_term = tripal_load_term_entity([
+      'vocabulary' => 'OBI',
+      'accession' => '0100026',
+    ]);
+    $org_bundle = tripal_load_bundle_entity(['term_id' => $org_term->id]);
+
+
+    $features = factory('chado.feature', 10)->create([
+      'organism_id' => $organism->organism_id,
+      'type_id' => $mrna_id,
+    ]);
+
+    $query = db_select('chado.cvterm', 'cvt');
+    $query->fields('cvt', ['cvterm_id']);
+    $query->join('chado.cv', 'cv', 'cvt.cv_id = cv.cv_id');
+    $query->join('chado.dbxref', 'dbx', 'dbx.dbxref_id = cvt.dbxref_id');
+    $query->fields('dbx', ['accession']);
+    $query->join('chado.db', 'db', 'db.db_id = dbx.db_id');
+    $query->fields('db', ['name']);
+    $query->condition('cv.name', 'sequence');
+
+    $terms = $query->execute()->fetchAll();
+    //oops we need an indexed array of these things instead in the API.
+    $terms_array = [];
+    foreach ($terms as $term) {
+      $terms_array[] = [
+        'cvterm_id' => $term->cvterm_id,
+        'accession' => $term->accession,
+        'vocabulary' => [
+          'short_name' => $term->name,
+        ],
+      ];
+    }
+
+    $terms_index = [];
+    $i = 0;
+    foreach ($features as $feature) {
+      $term = $terms[$i];
+
+      factory('chado.feature_cvterm', 1)->create([
+        'feature_id' => $feature->feature_id,
+        'cvterm_id' => $term->cvterm_id,
+      ]);
+      $terms_index[$term->cvterm_id] = $term->name . ':' . $term->accession;
+      $i++;
+    }
+
+
+    $this->publish('organism');
+    $this->publish('feature');
+
+    //populate the index
+    tripal_cv_xray_cvterm_entity_job();
+
+
+    //look up organism we published
+    $query = db_select('public.chado_' . $org_bundle->name, 'CE');
+    $query->fields('CE', ['entity_id']);
+    $query->condition('record_id', $organism->organism_id);
+    $organism_ent_id = $query->execute()->fetchField();
+
+    $lookup = tripal_cv_xray_lookup_entities_for_terms_count($terms_array, $mrna_bundle->id, $organism_ent_id);
+    $this->assertNotEmpty($lookup);
+    //key is db:accession
+    reset($terms_index);
+    $first_key = key($terms_index);
+     $accession = $terms_index[$first_key];
+     $this->assertNotEquals(0, $lookup[$accession]);//this term should have been used by our entity, so it better be in the lookup
+  }
+}
